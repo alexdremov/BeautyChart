722482fb3727aea461f7188e4acf84656279614c
aleksandrdremov <dremov.me@gmail.com>
2020-08-25 12:02:55 +0300

    New functionality added

--- Example/BeautyExample/ContentView.swift
+++ Example/BeautyExample/ContentView.swift
@@ -10,20 +10,16 @@ import SwiftUI
 import BeautyChart
 
 struct ContentView: View {
-
+    
+    @State var points:[CGPoint] = [CGPoint(x: 0, y: 17),
+                                   CGPoint(x: 1, y: 23),]
+    
+    @State var style:LineViewStyle = LineViewStyle().mode2()
     var body: some View {
-        VStack{
-            ChartSmoothView(points: [
-                CGPoint(x: 0, y: 17),
-                CGPoint(x: 1, y: 23),
-                CGPoint(x: 2, y: 60),
-                CGPoint(x: 3, y: 32),
-                CGPoint(x: 4, y: 12),
-                CGPoint(x: 5, y: 37),
-                CGPoint(x: 6, y: 0),
-                CGPoint(x: 7, y: 23),
-                CGPoint(x: 8, y: 60),
-            ], style: LineViewStyle().mode2())
+        self.style.darkTheme.bezierStepMode = false
+        self.style.lightTheme.bezierStepMode = false
+        return ScrollView{ VStack{
+            ChartSmoothView(points: points, title: "So volatile", caption: "Random points", style: self.style)
             
             ChartSmoothView(points: [
                 CGPoint(x: 0, y: 17),
@@ -35,9 +31,22 @@ struct ContentView: View {
                 CGPoint(x: 6, y: 0),
                 CGPoint(x: 7, y: 23),
                 CGPoint(x: 8, y: 60),
-            ], style: LineViewStyle())
+            ], title: "This one is sMoOtH", style: LineViewStyle())
+            HStack{
+                ChangeBarView(changePercent: 0.3, title: "Style")
+                ChangeBarView(changePercent: -0.73233, title: "Beauty")
+            }
+            ChangeBarView(changePercent: 0.9333, title: "Appreciation", caption: "Based on poll")
             Spacer()
-        }.padding()
+            }.padding()
+        }.onAppear(){
+                self.points = []
+                for i in 0...30{
+                    self.points.append(CGPoint(x:i*100,y:Int(arc4random()%100)))
+                }
+                
+                self.style.lightTheme.bezierStepMode = false
+        }
     }
 }
 
